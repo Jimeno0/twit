91f16be6d984e21a26d700a21194d69ddd8adb59
Jimeno0 <carlospj@icloud.com>
2016-11-02 00:09:54 +0100

    saving twits in files

--- app.rb
+++ app.rb
@@ -8,12 +8,15 @@ enable(:sessions)
 
 get '/' do
   @msg = "invalid user or passsword"
-  session[:twits] ||= []
+  # session[:twits] ||= []
   erb(:index)
 end
 
 post '/crear_twit' do
-  session[:twits].push(params[:message])
+  # session[:twits].push(params[:message])
+  twits_file = File.open("private/#{session[:user]}.txt", "a")
+  twits_file.puts("#{params[:message]}\n")
+  twits_file.close
   redirect to ("/my_page")
 end
 
@@ -44,17 +47,28 @@ post '/register_or_loggin' do
     file = File.open("private/passwords.txt", "a")
     file.puts("#{@user} #{@pass}\n")
     file.close
+
+    twits_file = File.open("private/#{@user}.txt", "a")
+    twits_file.close
+    
     session[:logged] = true
     session[:user] = @user
     redirect to("/my_page")
-    #Recorrer el txt para ver si ya esta el usuario?
-  end
-  
+  end  
 end
 
 
 get "/my_page" do
+  @twits = []
+  twits_file = File.open("private/#{session[:user]}.txt", "r")
+  twits_file.each_line do |line|
+    @twits.push(line.chomp)
+  end
+  twits_file.close
   erb(:main)
 end
 
-
+get "/logout" do
+  session[:logged] = false
+  redirect to("/")
+end
